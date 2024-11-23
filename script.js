ac44b3e06e398c3784defabc270531093cbc91a4
taham-dev <taham_2010@outlook.com>
2024-11-23 12:42:14 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ let ul = document.getElementById("list");
 addBtn.addEventListener("click", () => {
 	let liValue = document.createElement("li");
 	liValue.textContent = todoInput.value;
-
+    liValue.style.backgroundColor = "rgb(13, 84, 100)";
+    liValue.style.marginBottom = "10px"
+    liValue.style.borderRadius = "5px";
+    liValue.style.textAlign = "center"
+    liValue.style.paddingLeft = "5px"
 	document.getElementById("listValue").value = " ";
 
 	let deleteBtn = document.createElement("button");
-	deleteBtn.style.marginLeft = "10px";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.style.backgroundColor = "rgb(224, 5, 24)"
 	deleteBtn.textContent = "❌";
+    deleteBtn.classList.add("deleteBtn")
 
 	let checkBtn = document.createElement("button");
 	checkBtn.textContent = "✔️";
+    checkBtn.classList.add("checkBtn")
 
 	function removeList() {
 		ul.removeChild(liValue);
@@ -23,6 +30,8 @@ addBtn.addEventListener("click", () => {
 		liValue.style.textDecoration = "line-through";
 		liValue.style.paddingRight = "5px";
 		checkBtn.textContent = "Un-check";
+        checkBtn.style.backgroundColor = ""
+        
 		checkBtn.removeEventListener("click", checkList);
 		checkBtn.addEventListener("click", unCheck);
 	}
