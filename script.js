9a6998ada9a0ab2851c8c5b0465cd9fded1e5c46
taham-dev <taham_2010@outlook.com>
2024-11-23 15:52:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,9 +46,9 @@ addBtn.addEventListener("click", () => {
 	deleteBtn.addEventListener("click", removeList);
 	checkBtn.addEventListener("click", checkList);
 
-	document.getElementById("lista").appendChild(deleteBtn);
-	document.getElementById("lista").appendChild(checkBtn);
-	ul.appendChild(liValue);
+	listValue.appendChild(deleteBtn);
+	listValue.appendChild(checkBtn);
+	listValue.appendChild(liValue);
 });
 
 addBtn.addEventListener("click", addList);
